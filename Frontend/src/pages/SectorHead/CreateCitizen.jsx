5ba5fb235b1cc27281baa6ed36eb84cf1deb5181
Ayush Bhatia <151399480+Ayush-200@users.noreply.github.com>
2025-09-17 23:09:03 +0530

    Update CreateCitizen.jsx

--- Frontend/src/pages/SectorHead/CreateCitizen.jsx
+++ Frontend/src/pages/SectorHead/CreateCitizen.jsx
@@ -28,7 +28,7 @@ export default function CreateCitizen({ sectorName, onClose }) {
     const token = localStorage.getItem("token");
     try {
       const res = await axios.post(
-        "https://civicconnect-backend.onrender.com/api/citizen/register",
+        "https://hardik-project-backend-n2av.onrender.com/api/citizen/register",
         {
           ...formData,
           sector: sectorName
